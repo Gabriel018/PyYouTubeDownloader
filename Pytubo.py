68c842781a1b06a69d45031976a3494dd8e9ce96
GabrielDuke <42377719+Gabriel018@users.noreply.github.com>
2021-02-22 19:56:50 -0300

    1.1

--- Pytubo.py
+++ Pytubo.py
@@ -1,5 +1,39 @@
-from pytube import Youtube
+from tqdm import  tqdm
+from time import sleep
 
-p1 = Youtube("https://www.youtube.com/watch?v=YXdOAUKCc0k")
+from pytube import *
 
-p1.streams.first().download()
+
+
+
+
+#Video unico
+
+previoprogress = 0
+#Mostra o progresso do download
+def progress(stream,chunk,bytes_remainig):
+    global  previoprogress
+    total_size  = stream.filesize
+    bytes_download = total_size - bytes_remainig
+
+    liveprogress =  (int)(bytes_download/ total_size * 100)
+    if liveprogress > previoprogress:
+        previoprogress = liveprogress
+        print(liveprogress)
+p1 = YouTube("https://www.youtube.com/watch?v=Z-VfaG9ZN_U")
+p1.register_on_progress_callback(progress)
+p1.streams.first().download("c:/Users/User/Desktop\study")
+
+
+
+
+#Apenas Audio
+#p1.streams.get_audio_only().download("c:/Users/User/Desktop/Music_2/Guns_n_Roses")
+
+# Playlist
+#p2 = Playlist("https://www.youtube.com/watch?fbclid=IwAR1jLStRwdzHUfLo5Jb_H3WWD-YGPVVmSCr71oisfamYaxKOBugDlbfo4SI&v=-4pXPDO9PTU&feature=youtu.be")
+
+#for video in p2.videos:
+    #video.streams.get_audio_only().download("c:/Users/User/Desktop/study")
+
+
